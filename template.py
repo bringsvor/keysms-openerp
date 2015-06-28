2234af01c6104a0ea048fed34a0ab4a0cb9ac865
Torvald B. Bringsvor <torvald@bringsvor.com>
2015-06-28 18:24:54 +0200

    Update to new api.

--- template.py
+++ template.py
@@ -2,11 +2,8 @@ from KeySMS import KeySMS
 
 __author__ = 'tbri'
 
-from openerp.osv import osv, fields
-from openerp import tools
-from openerp.tools.translate import _
+from openerp import models, fields, api, _
 from urllib import urlencode, quote as quote
-
 import logging
 
 # Taken from email_template
@@ -42,39 +39,30 @@ try:
 except ImportError:
     _logger.warning("jinja2 not available, templating features will not work!")
 
-class sms_template(osv.osv):
+class sms_template(models.Model):
     _name = "keysms.template"
     _description = 'SMS Templates'
     _order = 'name'
 
-    _columns = {
-        'name': fields.char('Name'),
-        'model_id': fields.many2one('ir.model', 'Applies to', help="The kind of document with with this template can be used"),
-        'model': fields.related('model_id', 'model', type='char', string='Related Document Model',
-                                size=128, select=True, store=True, readonly=True),
-        'lang': fields.char('Language',
+    name = fields.Char('Name')
+    model_id = fields.Many2one('ir.model', 'Applies to', help="The kind of document with with this template can be used")
+    model = fields.Char(string='Related Document Model', related='model_id.model',
+                                size=128, select=True, store=True, readonly=True)
+    lang = fields.Char('Language',
                             help="Optional translation language (ISO code) to select when sending out an email. "
                                  "If not set, the english version will be used. "
                                  "This should usually be a placeholder expression "
                                  "that provides the appropriate language code, e.g. "
                                  "${object.partner_id.lang.code}.",
-                            placeholder="${object.partner_id.lang.code}"),
-        'body': fields.text('Body', translate=True, help="Text version of the message (placeholders may be used here)"),
-        }
+                            placeholder="${object.partner_id.lang.code}")
+    body = fields.Text('Body', translate=True, help="Text version of the message (placeholders may be used here)")
 
-    def render(self, cr, uid, template_id, model, res_id, context=None):
-        template = self.browse(cr, uid, template_id, context)
-        template_str = tools.ustr(template.body)
-        record = None
-        if res_id:
-            proxy = self.pool.get(model)
-            if not proxy:
-                _logger.error("Can't find model: %s", model)
-            else:
-                #record = proxy.browse(cr, uid, res_id, context=context)
-                record = res_id
+    def render(self, record):
+        #template = self.browse(cr, uid, template_id, context)
+        #t#emplate_str = tools.ustr(template.body)
 
-        user = self.pool.get('res.users').browse(cr, uid, uid, context=context)
+        user = None # self.pool.get('res.users').browse(cr, uid, uid, context=context)
+        context = self._context
 
         variables = {
                 'object': record,
@@ -83,6 +71,7 @@ class sms_template(osv.osv):
             }
 
 
+        template_str = self.body #TODO Possibly add translation.
         result = mako_template_env.from_string(template_str).render(variables)
         print "RESULT RENDER", result
         return result
@@ -96,15 +85,21 @@ class sms_template(osv.osv):
         keysms.auth(userid, apikey)
 
 
-    def send_sms(self, cr, uid, message, receivers, context = None):
+    def send_sms(self, message, receivers):
         # Find config
-        config_ids = self.search(cr, uid,  [], context=context)
+        #config_ids = self.search(cr, uid,  [], context=context)
 
-        assert len(config_ids) == 1
-        for cfg in self.browse(cr, uid, config_ids, context = context):
-            keySms = KeySMS()
-            self.get_paths(cr, uid, keySms)
-            #keySms.auth(cfg.userId, cfg.apiKey)
-            response = keySms.sms(message, receivers)
-            if not response['ok']:
-                raise Exception('Error while sending SMS to %s : %s' % (repr(receivers), repr(response)))
+        #assert len(config_ids) == 1
+        #for cfg in self.browse(cr, uid, config_ids, context = context):
+        keySms = KeySMS()
+        self.get_paths(keySms)
+        #keySms.auth(cfg.userId, cfg.apiKey)
+        response = keySms.sms(message, receivers)
+        if not response['ok']:
+            raise Exception('Error while sending SMS to %s : %s' % (repr(receivers), repr(response)))
+
+    @api.one
+    def render_and_send(self, record, receivers):
+        message = self.render(record)
+        print "MESSAGE", message
+        self.send_sms(message, receivers)
